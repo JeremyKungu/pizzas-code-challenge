2f537c758df18e7b3a84bcce182b4ca1f7afb1e1
JeremyKungu <jeremikungu27@gmail.com>
2022-11-30 12:03:17 +0300

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Pizza.create!(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
+Pizza.create!(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
+
+Restaurant.create!(name: "Sottocasa NYC", address: "298 Atlantic Ave, Brooklyn, NY 11201")
+Restaurant.create!(name: "PizzArte", address: "69 W 55th St, New York, NY 10019")
